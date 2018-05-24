ea1432c7d02dc4e0aed6756ef98e8cd250f4d865
Ahamed <ahamedali95@gmail.com>
2018-05-24 13:19:10 -0400

    done

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,25 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  const tasks = document.getElementById("tasks");
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const description = formData.get("new-task-description");
+    const color = formData.get("color");
+    const taskList = new TaskList(description, color);
+    tasks.appendChild(taskList.render());
+    input.value = "";
+  });
 });
+
+function myFunc(e) {
+  const button = e.getAttribute("data-description");
+  debugger;
+  for(let i = 0; i < tasks.length; i++) {
+
+  }
+}
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,18 @@
+let taskList = 0;
+
 class TaskList {
+  constructor(description, color) {
+    this.id = ++taskList;
+    this.description = description;
+    this.color = color;
+  }
+
+  render() {
+    const listItem = document.createElement("li");
+    listItem.setAttribute("id", this.id);
+    listItem.setAttribute("style", `color:${this.color}`);
+    listItem.innerHTML = this.description + `<button data-description="${this.description}" onclick="myFunc(this)">x</button>`;
+
+    return listItem;
+  }
 }
